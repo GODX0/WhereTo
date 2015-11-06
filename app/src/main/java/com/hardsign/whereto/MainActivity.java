3ba13945832efc35c49011ddbca82c3ffe74e358
GODX0 <mrjustfunky@mail.ru>
2015-11-06 16:21:21 +0300

    ГлавныйСписок

--- app/src/main/java/com/hardsign/whereto/MainActivity.java
+++ app/src/main/java/com/hardsign/whereto/MainActivity.java
@@ -2,36 +2,24 @@ package com.hardsign.whereto;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
-import android.view.Menu;
-import android.view.MenuItem;
+import android.widget.ArrayAdapter;
+import android.widget.ListView;
 
 public class MainActivity extends AppCompatActivity {
+
+    private ListView mainMenuList;
+    String[] names = { "Куда пойти c... ?", "Где отпразновать?", "Предложить событие"};
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
-    }
 
-    @Override
-    public boolean onCreateOptionsMenu(Menu menu) {
-        // Inflate the menu; this adds items to the action bar if it is present.
-        getMenuInflater().inflate(R.menu.menu_main, menu);
-        return true;
-    }
+        mainMenuList=(ListView)findViewById(R.id.listView);
 
-    @Override
-    public boolean onOptionsItemSelected(MenuItem item) {
-        // Handle action bar item clicks here. The action bar will
-        // automatically handle clicks on the Home/Up button, so long
-        // as you specify a parent activity in AndroidManifest.xml.
-        int id = item.getItemId();
+        ArrayAdapter<String> adapter = new ArrayAdapter<String>(this,
+                android.R.layout.simple_list_item_1, names);
 
-        //noinspection SimplifiableIfStatement
-        if (id == R.id.action_settings) {
-            return true;
-        }
-
-        return super.onOptionsItemSelected(item);
+        mainMenuList.setAdapter(adapter);
     }
 }
