4a8a0dc30bea7f159cd7c38e26eac53a8fce3bd2
GODX0 <mrjustfunky@mail.ru>
2015-11-07 18:44:20 +0300

    Новый дизайн

--- app/src/main/java/com/hardsign/whereto/ListPlacesActivity.java
+++ app/src/main/java/com/hardsign/whereto/ListPlacesActivity.java
@@ -1,9 +1,21 @@
 package com.hardsign.whereto;
 
+import android.os.AsyncTask;
 import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
+import android.util.Log;
 import android.widget.ImageView;
 import android.widget.TextView;
+
+import org.json.JSONArray;
+import org.json.JSONException;
+import org.json.JSONObject;
+
+import java.io.BufferedReader;
+import java.io.InputStream;
+import java.io.InputStreamReader;
+import java.net.HttpURLConnection;
+import java.net.URL;
 
 /**
  * Created by User on 06.11.2015.
@@ -11,6 +23,7 @@ import android.widget.TextView;
 public class ListPlacesActivity extends AppCompatActivity {
     private TextView myTextView;
     private ImageView imgView;
+    public static String LOG_TAG = "my_log";
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -21,5 +34,74 @@ public class ListPlacesActivity extends AppCompatActivity {
 
         ImageManager IM=new ImageManager();
         IM.fetchImage("http://dip-games.3dn.ru/hackathon/4mini.jpg",imgView);
+
+        new ParseTask().execute();
+
+
     }
+    private class ParseTask extends AsyncTask<Void, Void, String> {
+
+        HttpURLConnection urlConnection = null;
+        BufferedReader reader = null;
+        String resultJson = "";
+
+        @Override
+        protected String doInBackground(Void... params) {
+            // получаем данные с внешнего ресурса
+            try {
+                URL url = new URL("http://kudavl.ru/adapter.php");
+
+                urlConnection = (HttpURLConnection) url.openConnection();
+                urlConnection.setRequestMethod("GET");
+                urlConnection.connect();
+
+                InputStream inputStream = urlConnection.getInputStream();
+                StringBuffer buffer = new StringBuffer();
+
+                reader = new BufferedReader(new InputStreamReader(inputStream));
+
+                String line;
+                while ((line = reader.readLine()) != null) {
+                    buffer.append(line);
+                }
+
+                resultJson = buffer.toString();
+
+            } catch (Exception e) {
+                e.printStackTrace();
+            }
+            return resultJson;
+        }
+
+        @Override
+        protected void onPostExecute(String strJson) {
+            super.onPostExecute(strJson);
+            // выводим целиком полученную json-строку
+            Log.d(LOG_TAG, strJson);
+
+            JSONObject dataJsonObj = null;
+            String eventName = "";
+
+            try {
+                JSONArray jArray = new JSONArray(strJson);
+
+                JSONObject json_data=null;
+                for(int i=0;i<jArray.length();i++)
+                {
+                    //Log.d(LOG_TAG, jArray.getJSONObject(i));
+                   //json_data = jArray.getJSONObject(i);
+
+                   // String name = json_data.getString("Name");
+                    //String description = json_data.getString("Description");
+
+                  //  Log.d(LOG_TAG, "name: " + name);
+                  //  Log.d(LOG_TAG, "description: " + description);
+                }
+
+            } catch (JSONException e) {
+                e.printStackTrace();
+            }
+        }
+    }
+
 }
--- app/src/main/java/com/hardsign/whereto/MainActivity.java
+++ app/src/main/java/com/hardsign/whereto/MainActivity.java
@@ -5,56 +5,20 @@ import android.content.DialogInterface;
 import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.LayoutInflater;
 import android.view.View;
 import android.widget.AdapterView;
 import android.widget.ArrayAdapter;
+import android.widget.CheckBox;
 import android.widget.ListView;
 import android.widget.Toast;
 
 public class MainActivity extends AppCompatActivity {
 
-    private ListView mainMenuList;
-    String[] names = { "Куда пойти c... ?", "Где отпразновать?", "Предложить событие"};
-
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_main);
+        setContentView(R.layout.activity_listplaces);
 
-        mainMenuList=(ListView)findViewById(R.id.listView);
-
-        ArrayAdapter<String> adapter = new ArrayAdapter<String>(this,
-                android.R.layout.simple_list_item_1, names);
-
-        mainMenuList.setAdapter(adapter);
-
-        mainMenuList.setOnItemClickListener(new AdapterView.OnItemClickListener() {
-            public void onItemClick(AdapterView<?> parent, View view,
-                                    int position, long id) {
-                if (id == 0) {
-                    startActivity(new Intent(MainActivity.this, ListPlacesActivity.class));
-                }
-
-                if (id == 1) {
-                    Toast toast = Toast.makeText(getApplicationContext(),
-                            "В разработке!", Toast.LENGTH_SHORT);
-                    toast.show();
-                }
-                if (id == 2) {
-                    AlertDialog.Builder builder = new AlertDialog.Builder(MainActivity.this);
-                    builder.setTitle("Важное сообщение!")
-                            .setMessage("Тут будут правила для предложения события")
-                            .setCancelable(false)
-                            .setNegativeButton("Принимаю",
-                                    new DialogInterface.OnClickListener() {
-                                        public void onClick(DialogInterface dialog, int id) {
-                                            dialog.cancel();
-                                        }
-                                    });
-                    AlertDialog alert = builder.create();
-                    alert.show();
-                }
-            }
-        });
     }
 }
